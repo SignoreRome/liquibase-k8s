e31366cd75bc04b346bce47b86586c12b9250e75
SignoreRome <brochilling@yandex.ru>
2024-08-22 22:10:29 +0700

    init

---
+++ src/main/java/se/rocketscien/liquibasek8s/LiquibaseK8sApplication.java
@@ -0,0 +1,13 @@
+package se.rocketscien.liquibasek8s;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiquibaseK8sApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiquibaseK8sApplication.class, args);
+	}
+
+}
---
+++ src/test/java/se/rocketscien/liquibasek8s/LiquibaseK8sApplicationTests.java
@@ -0,0 +1,13 @@
+package se.rocketscien.liquibasek8s;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiquibaseK8sApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
